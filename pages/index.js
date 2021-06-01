79ba3029e904ed318b705eb4f24b6ca1f17b4a27
Khegyi <khegyi.balint@gmail.com>
2021-06-01 14:05:33 +0200

    first init

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+// import React, { useCallback, useEffect, useRef, useState } from 'react';
+
+const AppLayout = () => {
+
+  return (
+  <div>
+    <h1>Hello World</h1>
+  </div>
+  );
+};
+
+export default AppLayout;
